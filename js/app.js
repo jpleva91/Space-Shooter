63a51bc85657d233346f095d69b748c86d19c76b
jpleva91 <jpleva91@gmail.com>
2017-09-27 15:48:20 -0600

    adds player movement

--- js/app.js
+++ js/app.js
@@ -11,6 +11,27 @@ var mCtx = canvasMain.getContext('2d');
 var canvasShip = document.getElementById('ship');
 var sCtx = canvasShip.getContext('2d');
 
+
+// - Key Codes -
+window.addEventListener('keydown', this.keyCode, false);
+
+function keyCode(e) {
+	var code = e.keyCode;
+	if(code == 38){
+		var moveUp = sCtx.translate(0, -6);
+	}
+	if(code == 40) {
+		var moveDown = sCtx.translate(0, 6);
+	}
+	if(code == 37) {
+		var moveLeft = sCtx.translate(-6, 0);
+	}
+	if(code == 39) {
+		var moveRight = sCtx.translate(6, 0);
+	}
+
+}
+
 function starField() {
 
 	bCtx.fillStyle = "black";
@@ -25,6 +46,7 @@ function starField() {
 function render() {
 	requestAnimationFrame(render);
 
+	sCtx.clearRect(0,0,300,300);
 	// Paint Ship
 	makeShip();
 
